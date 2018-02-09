7dc2be859e665a64ae8198abfb9e19aec0985258
Flynn Platt <flynn.platt@jpl.nasa.gov>
2018-02-09 12:08:15 -0800

    3d only example initialized

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,13 +1,11 @@
 /**
- * Copyright 2017 California Institute of Technology.
- *
  * This source code is licensed under the APACHE 2.0 license found in the
  * LICENSE.txt file in the root directory of this source tree.
  */
 
 import { combineReducers } from "redux";
 import view from "_core/reducers/view";
-import map from "_core/reducers/map";
+import map from "reducers/map";
 import settings from "_core/reducers/settings";
 import help from "_core/reducers/help";
 import layerInfo from "_core/reducers/layerInfo";
